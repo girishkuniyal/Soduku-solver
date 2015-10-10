aadcbd1b979ca2d00d3f6fe9fb4dd65b2bdb1aae
unknown <girishchandrakuniyal@gmail.com>
2015-10-10 03:47:52 -0700

    user input code inserted for 6x6 matrix

--- source.cpp
+++ source.cpp
@@ -1,7 +1,60 @@
 #include<iostream>
+#include<windows.h>
 using namespace std;
+void gotoxy(int,int);
+                            /*starting of main function*/
 int main()
 {
-int soduku[9][9];
+int soduku[6][6];
+int i,j; //for loops;
+int x,y; //for cursor position on screen;
+case1:   //label for goto syntax if user entered wrong input then start with this label;
+{
+    x=0;
+    y=1;
+    gotoxy(0,0);
+    cout<<"enter element of matrix continiously and enter 0 for non-determined value\n";
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<6;j++)
+        {
+            gotoxy(x,y);
+            cin>>soduku[i][j];
+            x=x+3;
+        }
+        x=0;
+        y++;
+    }
+    x=10;
+    y=12;
+    cout<<"\n\t\t\t\t\t  Your entered soduku matrix is:\n";
+    for(i=0;i<6;i++)
+    {
+        gotoxy(30,y);
+        for(j=0;j<6;j++)
+        {
+
+            cout<<"| "<<soduku[i][j]<<" |";
+
+        }
+    y++;
+    }
+}
+    cout<<"\n\n\t\t\t\t\t\t!!!Message!!!\n\t\t\tIf entered soduku is correct press 1 and If incorrect press 2 \n";
+    cin>>x;
+if(x==2)
+{
+   system("cls");
+    goto case1;
+}
+
 return 0;
+}
+
+void gotoxy(int row,int column)
+{
+    COORD coord;
+    coord.X=row;
+    coord.Y=column;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
